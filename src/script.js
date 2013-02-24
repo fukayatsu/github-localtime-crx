0d7586054fa578663d2dda1aa5108922b58e5c23
fukayatsu <fukayatsu@gmail.com>
2013-02-24 19:22:14 +0900

    delete invalid arg

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 $("time.js-relative-date").each(function(){
   var originalText = $(this).text();
   var date         = moment($(this).attr("datetime"));
-  var dateStr      = date.format("YYYY-MM-DD ddd HH:mm:ss", 'ja');
+  var dateStr      = date.format("YYYY-MM-DD ddd HH:mm:ss");
 
   $(this).text(originalText + " ( " + dateStr + " )");
 });
